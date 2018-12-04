715dfce5fc3e7626f268cd98bff7dc5be9a608f6
Chelsea Allred <chelsdallred@gmail.com>
2018-12-03 20:29:17 -0700

    update

--- js/index.js
+++ js/index.js
@@ -1,35 +1,62 @@
-//multiple cards
-function Card(question, answer) {
-    this.question = question;
-    this.answer = answer;
-  }
+document.addEventListener('DOMContentLoaded', () => {
 
-var front = document.getElementById("front");
-var back = document.getElementById("back");
-var flip = document.getElementById("flip");
+    let cardArray = [];
+    let addCard = document.querySelector('.add-card');
+    let overlay = document.querySelector('.overlay');
+    let closeMenu = document.querySelector('.close-menu');
+    let frontCard = document.querySelector('#front-card');
+    let backCard = document.querySelector('#back-card');
+    let cardForm = document.querySelector('.new-card-form');
+    let cardContainer = document.querySelector('.card-container');
+    let userFrontCard;
+    let userBackCard;
 
-var card1 = new Card(
-    "Lexical Environment",
-    "Where code sits in relation to any surrounding code",
-  );
-var card2 = new Card(
-    "Execution Context",
-    "How, Why, When, and Where code is executed",
-  );
-var card3 = new Card(
-    "JSON",
-    "JavaScript Object Notation, for storing objects and their enclosed data. Often referred to as Key Value Pairs",
-  );
+    addCard.addEventListener('click', () => {
+       overlay.style.display = "flex";
+       closeMenu.addEventListener('click', () => {
+           overlay.style.display = "none";
+       });
+    });
 
-var myCards = [card1, card2, card3]
-//front of card
-//questions
+    cardForm.addEventListener('submit', (event) => {
+        event.preventDefault(); //use this because we aren't using a server
+        //console.log("it works!");
+        let card = {
+            front: frontCard.value,
+            back: backCard.value
+        }
+        cardArray.push(card);
+        frontCard.value = "";
+        backCard.value = "";
+        // console.log(cardArray);
+        displayCards();
+    });
 
-//back of card
-//answers
+    function displayCards() {
+        cardContainer.innerHTML = "";
+        cardArray.forEach((card) => {
+            let listItem = document.createElement('li');
 
-//add a card
+            var card = document.createElement('div');
+            card.classList.add('card');
 
-//edit a card
+            let frontCardDiv = document.createElement('div');
+            frontCardDiv.classList.add('front-card');
+            let cardFrontTitle = document.createElement('p')
+            cardFrontTitle.innerHTML = card.front
+            frontCardDiv.appendChild(cardFrontTitle);
+            card.appendChild(frontCardDiv);
 
-//delete a card
+            let backCardDiv = document.createElement('div');
+            backCardDiv.classList.
+
+            listItem.appendChild(cardFrontTitle);
+            let cardBackAnswer = document.createElement('p');
+            cardBackAnswer.innerHTML = card.back;
+            listItem.appendChild(card);
+            cardContainer.appendChild(listItem);
+        });
+
+    }
+
+});
